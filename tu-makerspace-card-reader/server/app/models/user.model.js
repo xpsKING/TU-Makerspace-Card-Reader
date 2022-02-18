f663c0fa45517a2dd65d9f3aa3733b53c2500685
Bennett Hermanoff <bennetthermanoff@gmail.com>
2022-02-18 15:42:37 -0600

    Backend (minus logging) is done! look at user.routes.js to see availible api calls

--- tu-makerspace-card-reader/server/app/models/user.model.js
+++ tu-makerspace-card-reader/server/app/models/user.model.js
@@ -1,39 +1,60 @@
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define("user", {
-      name: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      splash: {
-        type: Sequelize.BIGINT
-      },
-      fabTech: {
-        type: Sequelize.BOOLEAN
-      },
-      mill: {
-        type: Sequelize.BOOLEAN
-      },
-      lathe: {
-        type: Sequelize.BOOLEAN
-      },
-      waterjet: {
-        type: Sequelize.BOOLEAN
-      },
-      wood1: {
-        type: Sequelize.BOOLEAN
-      },
-      wood2: {
-        type: Sequelize.BOOLEAN
-      },
-      metal1: {
-        type: Sequelize.BOOLEAN
-      },
-      metal2: {
-        type: Sequelize.BOOLEAN
-      },
+  const User = sequelize.define("user", {
+    name: {
+      type: Sequelize.STRING
+    },
+    id: {
+      type: Sequelize.BIGINT,
+      primaryKey: true
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    splash: {
+      type: Sequelize.BIGINT
+    },
+    admin: {
+      type: Sequelize.BOOLEAN
+    },
+    fabTech: {
+      type: Sequelize.BOOLEAN
+    },
+    mill: {
+      type: Sequelize.BOOLEAN
+    },
+    lathe: {
+      type: Sequelize.BOOLEAN
+    },
+    waterjet: {
+      type: Sequelize.BOOLEAN
+    },
+    wood1: {
+      type: Sequelize.BOOLEAN
+    },
+    wood2: {
+      type: Sequelize.BOOLEAN
+    },
+    metal1: {
+      type: Sequelize.BOOLEAN
+    },
+    metal2: {
+      type: Sequelize.BOOLEAN
+    },
+
+
+  }, {
+    hooks: {
+      beforeCreate: (user, options) => {
+        {
+          user.password = user.password && user.password != "" ? bcrypt.hashSync(user.password, 10) : "";
+        }
+      }
       
-    });
-    return User;
-  };
+    }
+  })
+  return User;
+};
--- tu-makerspace-card-reader/server/app/routes/user.routes.js
+++ tu-makerspace-card-reader/server/app/routes/user.routes.js
@@ -1,19 +1,32 @@
 module.exports = app => {
     const users = require("../controllers/user.controller.js");
     var router = require("express").Router();
-    // Create a new User
+    
+    // Create a new User,
+    //REQUIRES: name, splash, email (no @tulane), 
+    //id (unique int, should be card number), 
+    //user (username of authenticating fabTech/admin), 
+    //authPassword (password of authenticating fabTech/admin)
     router.post("/", users.create);
-    // Retrieve all users
+
+    // Retrieve all users, excludes password column (they are hashed but we shouldn't let anyone access them anyways)
     router.get("/", users.findAll);
-    // Retrieve all published users
+
+    // Retrieve all fabtech users, excludes password column. currently broken but not really needed anyways.
     router.get("/fabTech", users.findAllFabtechs);
+
     // Retrieve a single Users with id
     router.get("/:id", users.findOne);
+
     // Update a User with id
+    //REQUIRES user, authPassword of higher permission (fabtech cannot update admin acct etc.)
     router.put("/:id", users.update);
+
     // Delete a User with id
+    //REQUIRES user, authPassword of higher permission (fabtech cannot update admin acct etc.)
     router.delete("/:id", users.delete);
+
     // Delete all users
-    router.delete("/", users.deleteAll);
+    //router.delete("/", users.deleteAll); //def dont need this in the api
     app.use('/api/users', router);
   };
