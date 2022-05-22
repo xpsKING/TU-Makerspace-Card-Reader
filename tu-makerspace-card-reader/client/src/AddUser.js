07ba24a576d9db2b9222192edfe05cf1289df7f0
Maddie <67487326+MadBeignet@users.noreply.github.com>
2022-05-21 23:30:27 -0500

    Update AddUser.js

--- tu-makerspace-card-reader/client/src/AddUser.js
+++ tu-makerspace-card-reader/client/src/AddUser.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { addUser } from './APIRoutes.js';
-import './AddUser.css';
+import './addUser.css';
 import Inputs from './Inputs.js';
 import './DarkMode.css';
 
