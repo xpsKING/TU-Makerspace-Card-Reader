7e798f1755043e9a16073ca9724e71b1b3e84313
MadBeignet <maddie.wisinski@ymail.com>
2022-05-24 22:53:10 -0500

    password

--- tu-makerspace-card-reader/client/src/EditUser.js
+++ tu-makerspace-card-reader/client/src/EditUser.js
@@ -29,6 +29,7 @@ function RenderEditPassword(props) {
         return (
             <div>
                 <Inputs
+                    type="password"
                     className="BoxInput"
                     placeholder="Create Password"
                     value={props.createdPassword}
