aeaff418a5c1c85dbe171e5f73b6669223d73ce1
Bennett Hermanoff <bennetthermanoff@gmail.com>
2022-05-25 22:43:46 -0500

    Added editMachine Axios route

--- tu-makerspace-card-reader/client/src/APIRoutes.js
+++ tu-makerspace-card-reader/client/src/APIRoutes.js
@@ -85,3 +85,15 @@ export function editUser(id, updatedUser, user, authPassword) {
         }
     }
 }
+export function editMachine(machineId, updatedMachine, authUser, authPassword) {
+    return {
+        method: 'put',
+        data:{updatedMachine: updatedMachine, user: authUser, authPassword: authPassword},
+        url: 'https://localhost:8443/api/machines/' + machineId,
+        proxy: {
+            host: 'localhost',
+            port: 8443,
+
+        }
+    }
+}
