f50f14f2bd03881fd764d939d228240c0a3e4068
MadBeignet <maddie.wisinski@ymail.com>
2022-05-25 23:21:53 -0500

    idk

--- tu-makerspace-card-reader/client/src/MachineView.js
+++ tu-makerspace-card-reader/client/src/MachineView.js
@@ -165,7 +165,7 @@ export default class MachineView extends React.Component {
 
         {/* Creates multiple machines from the machine[] state! Machine state is filled on component load and is called via api GET machines/group/groupname */}
         {/* Change the machinegroup prop when you render the search component to set which tablet this is run on  */}
-        <div className='container2'>
+        <div className='container3'>
           {this.state.machines.map((machine) => (
             <Machine
 
